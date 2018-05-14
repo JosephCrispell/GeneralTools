9f47e31128092459dd117f68fd6203b60e1ddd39
JosephCrispell <crispelljoseph@gmail.com>
2018-05-14 14:39:01 +0100

    TimeTrialUpdates 14-05-2018

--- Homoplasy/PlotTimeTrialResults_27-04-18.R
+++ Homoplasy/PlotTimeTrialResults_27-04-18.R
@@ -18,36 +18,32 @@ file <- paste(path, "timeTaken_11-05-18.pdf", sep="")
 pdf(file)
 
 # Create an empty plot on a log scale
-plot(table$NSequences, y=log(table$R), col="white", las=1, bty="n",
+plot(table$NSequences, y=table$R, col="white", las=1, bty="n",
      ylab="Time taken (seconds)", xlab="Number of sequences",
      main="Speed comparison between tools to identify homoplasies",
-     yaxt="n", xaxt="n", ylim=c(-1.1, max(log(table$R))))
+     xaxt="n", ylim=c(-1.1, max(table$R)))
 
 # Add the mean values across 10 replicates and range
 for(nSequences in seq(50, 500, 50)){
-  
+
   # Subset the table for the current value of sequences
   subset <- table[table$NSequences == nSequences, ]
-  
+
   # R
-  points(x=nSequences, y=log(mean(subset$R)), pch=19, col=rgb(0,0,0, 0.5))
-  points(x=c(nSequences, nSequences), y=log(range(subset$R)), type="l", col=rgb(0,0,0, 0.5))
-  
+  points(x=nSequences, y=mean(subset$R), pch=19, col=rgb(0,0,0, 0.5))
+  points(x=c(nSequences, nSequences), y=range(subset$R), type="l", col=rgb(0,0,0, 0.5))
+
   # Java
-  points(x=nSequences, y=log(mean(subset$Java)), pch=19, col=rgb(1,0,0, 0.5))
-  points(x=c(nSequences, nSequences), y=log(range(subset$Java)), type="l", col=rgb(1,0,0, 0.5))
-  
+  points(x=nSequences, y=mean(subset$Java), pch=19, col=rgb(1,0,0, 0.5))
+  points(x=c(nSequences, nSequences), y=range(subset$Java), type="l", col=rgb(1,0,0, 0.5))
+
   # TreeTime
-  points(x=nSequences, y=log(mean(subset$TreeTime)), pch=19, col=rgb(0,0,1, 0.5))
-  points(x=c(nSequences, nSequences), y=log(range(subset$TreeTime)), type="l", col=rgb(0,0,1, 0.5))
+  points(x=nSequences, y=mean(subset$TreeTime), pch=19, col=rgb(0,0,1, 0.5))
+  points(x=c(nSequences, nSequences), y=range(subset$TreeTime), type="l", col=rgb(0,0,1, 0.5))
 }
 
 # Add x axis
-axis(side=1, at=seq(0, 500, 50))
-
-# Add y axis (log scale)
-at <- c(1, 10, 30, 90)
-axis(side=2, at=c(-1.1,log(at)), labels=c(0, at), las=1)
+axis(side=1, at=seq(50, 500, 50))
 
 # Add legend
 legend("bottomright", legend=c("R", "Java", "TreeTime"), bty="n", text.col=c("black", "red", "blue"))
