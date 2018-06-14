579468997174e1e1c083a64a3eaf2cab50db4099
JosephCrispell <crispelljoseph@gmail.com>
2018-06-14 18:16:10 +0100

    GeneralUpdate 14-06-2018

--- GetConsensusFromClustalAlignment_28-09-17.R
+++ GetConsensusFromClustalAlignment_28-09-17.R
@@ -19,7 +19,7 @@ library(ape)
 ######################
 
 # Set the path
-path <- "C:/Users/Joseph Crisp/Desktop/"
+path <- "/home/josephcrispell/Desktop/"
 
 # Read in the sequences
 fastaFile <- paste(path, "test.fasta", sep="")
@@ -32,60 +32,44 @@ sequences <- read.FASTA(fastaFile)
 # Run the alignment
 alignment <- clustal(sequences)
 
-# Print out the file
-output <- paste(path, "alignment.fasta", sep="")
-write.dna(alignment, file = output, format = 'fasta', colsep="")
-
 ################################
 # Build the consensus sequence #
 ################################
 
-# Read in the alignment
-sequences <- readFASTA(output)
+# Add the consensus sequence into the alignment
+alignment <- getConsensus(alignment)
 
-# Get the consensus sequence
-consensusSequence <- getConsensus(sequences)
-
-# Print out the consensus sequence
-outputFile <- paste(path, "Consensus.fasta", sep="")
-printSequence(consensusSequence, outputFile)
+# Print out sequences with consensus
+outputFile <- paste(path, "sequencesWithConsensus.fasta", sep="")
+write.FASTA(alignment, file=outputFile)
 
 #############
 # FUNCTIONS #
 #############
 
-printSequence <- function(consensus, fileName){
-  
-  # Open an output file
-  connection <- file(fileName, "w")
-  
-  # Write a sequence header
-  writeLines(">Consensus", connection)
-  
-  # Write out the sequence
-  writeLines(paste(consensus, collapse=""), connection)
-  
-  # Close the output file
-  close(connection)
-}
-
 getConsensus <- function(sequences){
   
-  # Get the sequence names
-  sequenceNames <- names(sequences)
+  # Convert between ways of storing sequences
+  sequences <- as.alignment(sequences)
   
   # Initialise vector to store the consensus
   consensus <- c()
   
+  # Get the sequences and split them into their nucleotides
+  nucleotides <- list()
+  for(i in 1:sequences$nb){
+    nucleotides[[sequences$nam[i]]] <- strsplit(sequences$seq[i], split="")[[1]]
+  }
+  
   # Examine each position in the alignment
-  for(position in 1:length(sequences[[1]])){
+  for(position in 1:length(nucleotides[[1]])){
     
     # Initialise a vector to store the alleles at the current position
     alleles <- c()
     
     # Examine the current position in each sequence - store each allele
-    for(i in 1:length(sequenceNames)){
-      alleles[i] <- sequences[[sequenceNames[i]]][position]
+    for(i in 1:sequences$nb){
+      alleles[i] <- nucleotides[[i]][position]
     }
     
     # Get the number of each allele present
@@ -96,45 +80,17 @@ getConsensus <- function(sequences){
     if(length(maxAllele) == 1){
       consensus[position] <- maxAllele
     }else{
-      consensus[position] <- "N"
+      consensus[position] <- "n"
     }
   }
   
-  return(consensus)
-}
-
-readFASTA <- function(fastaFile){
+  # Attach the consensus to the alignment
+  sequences$seq[sequences$nb + 1] <- paste(consensus, collapse="")
+  sequences$nb <- sequences$nb + 1
+  sequences$nam <- c(sequences$nam, "consensus")
   
-  # Open the file and store lines
-  connection <- file(fastaFile, "r")
-  fileLines <- readLines(connection)
-  close(connection)
-  
-  # Initialise a list to store the sequences
-  sequences <- list()
-  
-  # Examine each of the file lines one by one
-  for(i in 1:length(fileLines)){
-    
-    # Check if line starts with ">"
-    if(grepl(fileLines[i], pattern="^>") == TRUE){
-      
-      # Store the previous sequence
-      if(i != 1){
-        sequences[[name]] <- strsplit(sequence, split="")[[1]]
-      }
-      
-      # Initialise variables to store sequence and id
-      name <- substr(fileLines[i], 2, stop=nchar(fileLines[i]))
-      sequence <- ""
-    }else{
-      
-      sequence <- paste(sequence, fileLines[i], sep="")
-    }
-  }
-  
-  # Store the last sequence
-  sequences[[name]] <- strsplit(sequence, split="")[[1]]
+  # Convert between ways of storing sequences
+  sequences <- as.DNAbin.alignment(sequences)
   
   return(sequences)
 }
