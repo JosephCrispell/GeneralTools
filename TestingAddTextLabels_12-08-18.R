d1945e6275c378e87fe5f6d18390b38eef77ab71
JosephCrispell <crispelljoseph@gmail.com>
2018-09-14 19:55:37 +0100

    GeneralUpdate 14-09-2018

--- TestingAddTextLabels_12-08-18.R
+++ TestingAddTextLabels_12-08-18.R
@@ -3,7 +3,8 @@ library(addTextLabels)
 # Create some random points
 n <- 50
 testLabels <- c("short", "mediummm", "looooonnnnnnngggggg", "0090292002", "9", "A Different label")
-coords <- data.frame(X=runif(n), Y=runif(n, min=0, max=100), Name=sample(testLabels, size=n, replace=TRUE))
+coords <- data.frame(X=runif(n), Y=runif(n, min=0, max=100), Name=sample(testLabels, size=n, replace=TRUE),
+                     stringsAsFactors = FALSE)
 
 # Plot them without labels
 plot(x=coords$X, y=coords$Y, pch=19, bty="n", xaxt="n", yaxt="n", col="red", xlab="X", ylab="Y")
