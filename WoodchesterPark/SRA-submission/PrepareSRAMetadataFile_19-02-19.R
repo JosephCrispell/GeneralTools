90726338a94ec361b5175e185f1853b587a052f4
JosephCrispell <crispelljoseph@gmail.com>
2019-02-20 12:25:54 +0000

    SRAsubmission 20-02-2019

--- WoodchesterPark/SRA-submission/PrepareSRAMetadataFile_19-02-19.R
+++ WoodchesterPark/SRA-submission/PrepareSRAMetadataFile_19-02-19.R
@@ -23,20 +23,124 @@ colnames(output) <- colnames(template)
 
 # Fill in the table with the sample (FASTQ) information
 output$bioproject_accession <- rep(bioProject, nrow(output))
-output$sample_name <- attributes$accession
+output$biosample_accession <- attributes$accession
 output$library_ID <- attributes$sample_name
 output$title <- rep("WGS for M. bovis from England", nrow(output))
 output$library_strategy <- rep("WGS", nrow(output))
 output$library_source <- rep("GENOMIC", nrow(output))
+output$library_selection <- rep("RANDOM", nrow(output))
 output$library_layout <- rep("paired", nrow(output))
 output$platform <- rep("ILLUMINA", nrow(output))
-output$instrument_model <- ifelse(grepl(attributes$sample_name, pattern="^WB"), "Illumina MiSeq", "Illumina HiSeq")
+output$instrument_model <- ifelse(grepl(attributes$sample_name, pattern="^AF"), "NextSeq 500", "Illumina MiSeq")
+output$design_description <- rep("Library preparation available on request", nrow(output))
 output$filetype <- rep("fastq", nrow(output))
 
-#### Add in the FASTQ file names and MD5 sums ####
+#### Add in the FASTQ file names ####
 
 # Get an array of the badger FASTQ files and select those used in analyses
+badgerFastqs <- c(getFilesInDirectory(paste0(path, "Badger_Batch1/"), "fastq.gz"),
+                      getFilesInDirectory(paste0(path, "Badger_Batch2/"), "fastq.gz"))
 
 # Get an array of the cattle FASTQ files and select those used in analyses
+cattleFastqs <- c(getFilesInDirectory(paste0(path, "Cattle_FASTQ_1stRound/"), "fastq.gz"),
+                      getFilesInDirectory(paste0(path, "Cattle_FASTQ_2ndRound/"), "fastq.gz"),
+                      getFilesInDirectory(paste0(path, "NewCattle_16-03-18/fastqs/"), "fastq.gz"))
 
-# md5sums?
+# Note the FASTQ files associated with each of the IDs that we are interested in
+fastqsForEachID <- noteFastqFilesForID(badgerFastqs, cattleFastqs, attributes)
+
+# Add in the FASTQ file names into the output table
+for(row in seq_len(nrow(output))){
+  output[row, c("filename", "filename2")] <- fastqsForEachID[[output[row, "library_ID"]]]
+}
+
+#### Create the output file ####
+
+# Write the output table to file
+outputFile <- paste0(path, "SRA_submission_19-02-19/SampleInfo_NCBI_19-02-19.tsv")
+write.table(output, file=outputFile, quote=FALSE, sep="\t", row.names=FALSE, na="")
+
+#### Copy the FASTQ files into a single directory ####
+
+# Create a directory for the files
+directory <- paste0(path, "SRA_submission_19-02-19/FASTQs/")
+dir.create(directory)
+
+# Copy the badger FASTQ files into the folder - only those that were used in analysis
+copyFASTQsInDirectoryToAnother(path, from="Badger_Batch1/", to=directory, attributes)
+copyFASTQsInDirectoryToAnother(path, from="Badger_Batch2/", to=directory, attributes)
+
+# Copy the cattle FASTQ files into the folder - only those that were used in analysis
+copyFASTQsInDirectoryToAnother(path, from="Cattle_FASTQ_1stRound/", to=directory, attributes)
+copyFASTQsInDirectoryToAnother(path, from="Cattle_FASTQ_2ndRound/", to=directory, attributes)
+copyFASTQsInDirectoryToAnother(path, from="NewCattle_16-03-18/fastqs/", to=directory, attributes)
+
+#### FUNCTIONS ####
+
+copyFASTQsInDirectoryToAnother <- function(path, from, to, attributes){
+  
+  # Get a list of the FASTQ files in the from directory
+  fastqs <- getFilesInDirectory(paste0(path, from), "fastq.gz")
+  
+  # Get the IDs from these FASTQ files
+  ids <- getIDsFromFiles(fastqs)
+  
+  # Copy only the used FASTQs into to directory
+  file.copy(paste0(path, from, fastqs[ids %in% attributes$sample_name]), to)
+}
+
+noteFastqFilesForID <- function(badgerFastqs, cattleFastqs, attributes){
+  
+  # Initialise a list to store the pair of FASTQs associated with each ID
+  fastqsForEachID <- list()
+  
+  # Examine the FASTQ files
+  for(fastq in c(badgerFastqs, cattleFastqs)){
+    
+    # Get the ID from the current FASTQ
+    id <- strsplit(fastq, split="_")[[1]][1]
+    
+    # Check that ID exists in attributes table
+    if(id %in% attributes$sample_name == FALSE){
+      next
+    }
+    
+    # Check if we have encountered the current ID
+    if(is.null(fastqsForEachID[[id]])){
+      fastqsForEachID[[id]] <- c(fastq)
+    }else{
+      fastqsForEachID[[id]] <- c(fastqsForEachID[[id]], fastq)
+    }
+  }
+  
+  return(fastqsForEachID)
+}
+
+getIDsFromFiles <- function(files){
+  
+  # Create an array to store the IDs
+  ids <- c()
+  
+  # Examine each of the forward fastq files
+  for(file in files){
+    
+    # Get the ID from the current file name
+    ids[length(ids) + 1] <- strsplit(file, split="_")[[1]][1]
+  }
+  
+  return(ids)
+}
+
+getFilesInDirectory <- function(directory, fileEnding=NULL){
+  
+  # List the files in the directory
+  files <- list.files(path=directory)
+  
+  # Select files with file ending if provided
+  if(is.null(fileEnding) == FALSE){
+    
+    files <- files[grepl(files, pattern=paste0(fileEnding, "$"))]
+  }
+  
+  return(files)
+}
