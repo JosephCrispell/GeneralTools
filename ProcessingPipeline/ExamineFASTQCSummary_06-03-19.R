fa0fe4b3fd91c081995ecbc111eb7362e768f0a7
JosephCrispell <crispelljoseph@gmail.com>
2019-10-31 11:39:44 +0000

    CreatingPipelineREADME 31-10-2019

--- ProcessingPipeline/ExamineFASTQCSummary_06-03-19.R
+++ ProcessingPipeline/ExamineFASTQCSummary_06-03-19.R
@@ -1,10 +1,10 @@
 #### Read in the data ####
 
 # Set the path variable
-path <- "/home/josephcrispell/Desktop/Research/EdgeArea_UK/Fastqs_24-09-19/Fastqc/"
+path <- "/home/josephcrispell/Desktop/Research/EdgeArea_UK/Fastqs_06-19_n68/FASTQC/"
 
 # Read in the FASTQC file summary table
-summaryFile <- paste0(path, "Fastqc_summary_23-10-19.txt")
+summaryFile <- paste0(path, "Fastqc_summary_30-10-19.txt")
 summary <- read.table(summaryFile, header=TRUE, sep="\t", stringsAsFactors=FALSE)
 
 # Sort the file names
@@ -12,6 +12,7 @@ summary <- summary[order(summary$FileName), ]
 
 # Look at GC distribution
 hist(summary$GC, breaks=20, las=1)
+summary[summary$GC != 65, c("FileName", "GC")]
 
 # Look at the left and right trimming
 boxplot(summary$LeftTrim, summary$RightTrim, names=c("LEFT", "RIGHT"), las=1, frame=FALSE,
@@ -22,7 +23,7 @@ table(summary$AdapterContentFlag)
 
 # Look at the number of reads distribution
 hist(summary$NumberReads, breaks=100, las=1)
-summary[which(summary$NumberReads < 250000), c("FileName", "NumberReads")]
+summary[summary$NumberReads < 250000, c("FileName", "NumberReads")]
 
 # Look at the read length distribution
 table(summary$ReadLength)
