1db17e2258d6d6faf19ab0848f16cf253513d5fe
Joseph Crispell <crispelljoseph@gmail.com>
2020-02-27 16:58:17 +0000

    Working on reading in BASTA results

--- WoodchesterPark/Daniel_SimulationOutput/ExaminingBASTAResults_27-02-20.R
+++ WoodchesterPark/Daniel_SimulationOutput/ExaminingBASTAResults_27-02-20.R
@@ -5,7 +5,7 @@ library(ape) # Reading in FASTA
 library(lubridate) # Converting to decimal dates
 
 # Set the path
-path <- file.path("~", "Desktop", "BuildingBASTAXML", "BASTA_equal_relaxed_25-02-20")
+path <- file.path("~", "Desktop", "BuildingBASTAXML")
 
 # Set the date
 date <- format(Sys.Date(), "%d-%m-%y")
@@ -19,28 +19,27 @@ genomeSize <- 4345492
 #### Read in the BASTA .log file ####
 
 # Note the files in BASTA directory
-files <- list.files(path)
+files <- list.files(file.path(path, "BASTA_equal_relaxed_25-02-20"), full.names=TRUE)
 
 # Read in the log table
-logFile <- file.path(path, files[grepl(files, pattern=".log")])
+logFile <- files[grepl(files, pattern=".log")]
 logTable <- readLogFile(logFile, burnInProp=0.1)
 
 #### Count transitions between demes estimated in .trees file ####
 
 # Note the path to the JAVA jar tool that counts transitions in .trees file
-pathToJarFile <- "/home/josephcrispell/Desktop/Research/Java/ExecutableJarFiles/CountTransitions_07-06-19.jar"
+pathToTransitionCountingJarFile <- file.path(path, "CountTransitions_07-06-19.jar")
 
 # Note the name of the .trees file
-treesFile <- file.path(path, files[grepl(files, pattern=".trees")])
+treesFile <- files[grepl(files, pattern=".trees")]
 
 # Count the number of transitions between demes
 countsFile <- paste0(substr(treesFile, 1, nchar(treesFile)-6), "_TransitionCounts.txt")
+countTransitionsOnPosteriorTrees(countsFile, logFile, treesFile, states, pathToTransitionCountingJarFile)
 
-I AM HERE!?!?!?!?!
+#### FUNCTIONS - transition counts ####
 
-#### FUNCTIONS - log file ####
-
-countTransitionsOnPosteriorTrees <- function(countsFile, logFile, treesFile, states){
+countTransitionsOnPosteriorTrees <- function(countsFile, logFile, treesFile, states, pathToJarFile){
   
   # Check that transition counts file doesn't exist
   if(file.exists(countsFile) == FALSE){
@@ -55,6 +54,59 @@ countTransitionsOnPosteriorTrees <- function(countsFile, logFile, treesFile, sta
     system(command, wait=TRUE)
   }
 }
+
+#### FUNCTIONS - log file ####
+
+summarisePosteriorLogTable <- function(path, logTable, code=2, arrowFactor, nBootstraps=1000,
+                                       genomeSize, date){
+    
+  # Open a pdf file
+  pdf(paste0(directory, analysis, "_ResultsSummary.pdf"))
+    
+  # Get the deme structure
+  demeStructure <- strsplit(analysis, "_")[[1]][1]
+    
+  # Get the log table
+  logTable <- logTables[[analysis]]
+    
+  # Note which parameters to examine posterior support for
+  colsToCalculateESS <- colnames(logTable)[
+    grepl(x=colnames(logTable), pattern="Sample|rateMatrixFlag|forward|Count") == FALSE]
+    
+  # Plot the ESS values of each parameter estimated
+  plotParameterESSValues(logTable, colsToCalculateESS)
+    
+  # Plot the posterior support for each deme as source
+  plotPosteriorSupportForEachDemeAsRoot(logTable, demeStructure)
+    
+  # Plot the population size estimates
+  plotPopulationSizes(logTable, demeStructure, alpha=0.5)
+    
+  # Examine the substitution rate estimates
+  examineSubstitutionRateEstimates(logTable, genomeSize)
+    
+  # Produce a migration rate estimation figure - weight by rate flags
+  # Diagrams designed with code = 2 (FORWARDS) in mind
+  migrationRateEstimates[[analysis]] <- 
+    plotMigrationRates(logTable, demeStructure, code, arrowFactor)
+    
+  # Plot the forward migration rate posterior distributions and their flags
+  plotMigrationRatePosteriors(logTable, demeStructure)
+    
+  # Plot the parameter traces
+  plotParameterTraces(logTable, colsToCalculateESS)
+    
+  # Calculate the acim
+  migrationRateEstimates[[analysis]][["AICM"]] <- 
+    calculateAICM(logTable$treeLikelihood1, nBootstraps)
+    
+    
+  # Close the pdf file
+  dev.off()
+  
+  return(migrationRateEstimates)
+}
+
 
 calculateForwardMigrationRates <- function(logTable){
   
