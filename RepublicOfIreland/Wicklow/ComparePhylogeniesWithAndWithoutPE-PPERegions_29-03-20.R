cf8272002edd7d3c71c2f2a9ce354435a1bd3291
JosephCrispell <crispelljoseph@gmail.com>
2020-04-04 14:09:23 +0100

    Finished comparing sequenes with/without PE-PPE

--- RepublicOfIreland/Wicklow/ComparePhylogeniesWithAndWithoutPE-PPERegions_29-03-20.R
+++ RepublicOfIreland/Wicklow/ComparePhylogeniesWithAndWithoutPE-PPERegions_29-03-20.R
@@ -40,22 +40,53 @@ tree <- ladderize(tree, right=FALSE)
 
 #### Plot a tangle plot to illustrate the differences between the phylogenies ####
 
-tanglePlot(tree, treeWithPEPPE, connectingLine.col="red", connectingLine.lwd=1.5, connectingLine.lty=2, offsetProp=0.065)
+# Open a PDF
+pdf(file.path(path, "Figures", paste0("tanglePlot_", date, ".pdf")))
 
-phyloHelpeR::addSNPScale(position="topright")
+# Plot the two phylogenies facing one another and highlight differences
+tanglePlot(tree, treeWithPEPPE, connectingLine.col="red", connectingLine.lwd=1.5, connectingLine.lty=2, offsetProp=0.08)
 
-#### Compare the patristic distances ####
+# Add a SNP scale
+addSNPScale(position="topright", lineWidth=1.5)
+
+# CLose the PDF
+dev.off()
+
+#### Compare the genetics distances ####
 
 # Read in the fasta file and generate genetic distance matrices
-sequences <- read.dna(fastaFilePEPPE, format="fasta")
-distancesWithPEPPE <- dist.dna(sequences)
-sequences <- read.dna(fastaFile, format="fasta")
-distances <- dist.dna(sequences)
+distancesWithPEPPE <- dist.dna(read.dna(fastaFilePEPPE, format="fasta"), model="N")
+distances <- dist.dna(read.dna(fastaFile, format="fasta"), model="N")
 
 # Calculate the correlation between the matrices (check https://stats.idre.ucla.edu/r/faq/how-can-i-perform-a-mantel-test-in-r/)
 results <- mantel.rtest(distancesWithPEPPE, distances, nrepet = 9999)
 
-plot(distances, distancesWithPEPPE, pch=19, col=rgb(0,0,0, 0.1), cex=0.5)
+## Plot the genetics distances against one another
+
+# Open a PDF
+pdf(file.path(path, "Figures", paste0("ComparingGeneticDistances_", date, ".pdf")))
+
+# Turn off scientific notation for lare/small numbers
+options(scipen=10) # Default is zero
+
+# Create y = x set of values
+range <- range(distances)
+values <- seq(range[1], range[2])
+
+# Create the initial plot with X=Y line
+plot(values, values, type="l", lty=2, lwd=2, col="red", bty="n", las=1,
+     xlab="Number of SNVs WITHOUT PE/PPE regions",
+     ylab="Number of SNVs WITH PE/PPE regions",
+     main="Comparing genetic distances calculated with/without PE/PPE regions")
+
+# Add the genetic distances
+points(distances[lower.tri(distances)], distancesWithPEPPE[lower.tri(distancesWithPEPPE)], pch=19, col=rgb(0,0,0, 0.05), cex=2)
+
+# Add the mantel.test results
+legend("topleft", legend=paste0("Mantel test observation: ", results$obs, " (p = ", round(results$pvalue, digits=5), ")"), bty="n")
+
+# Close the PDF
+dev.off()
 
 #### FUNCTIONS ####
 
