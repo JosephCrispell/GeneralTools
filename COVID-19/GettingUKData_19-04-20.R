ba18039cfc5198553120e4d47f4513bdfb9ecf6f
JosephCrispell <crispelljoseph@gmail.com>
2020-04-20 20:56:02 +0100

    Cleaning up dashboard code

--- COVID-19/GettingUKData_19-04-20.R
+++ COVID-19/GettingUKData_19-04-20.R
@@ -1,12 +1,12 @@
 ukCasesFile <- "https://raw.githubusercontent.com/tomwhite/covid-19-uk-data/master/data/covid-19-cases-uk.csv"
-cases <- read.csv(ukCasesFile, stringsAsFactors=FALSE, check.names=FALSE)
+ukCases <- read.csv(ukCasesFile, stringsAsFactors=FALSE, check.names=FALSE)
 
 # Convert case counts to numbers
-cases$TotalCases <- as.numeric(cases$TotalCases)
+ukCases$TotalCases <- as.numeric(ukCases$TotalCases)
 
 # Collapse to counts for each country on each date
-cases$CountryDate <- paste0(cases$Country, "_", cases$Date)
-countsPerCountry <- aggregate(cases$TotalCases, by=list(cases$CountryDate), FUN=sum, na.rm=TRUE)
+ukCases$CountryDate <- paste0(ukCases$Country, "_", ukCases$Date)
+countsPerCountry <- aggregate(ukCases$TotalCases, by=list(ukCases$CountryDate), FUN=sum, na.rm=TRUE)
 
 # Extract the country names and dates from aggregated table
 countsPerCountry$Date <- sapply(countsPerCountry$Group.1, FUN=function(countryWithDate){strsplit(countryWithDate, "_")[[1]][2]})
@@ -19,3 +19,13 @@ rownames(casesPerCountry) <- casesPerCountry$Date
 for(row in 1:nrow(countsPerCountry)){
   casesPerCountry[countsPerCountry[row, "Date"], countsPerCountry[row, "Country"]] <- countsPerCountry[row, "x"]
 }
+
+# Add a total column
+casesPerCountry[, "Grand Total"] <- rowSums(casesPerCountry[, 2:4])
+
+
+
+# Read in the UK shape file
+shapeFile <- file.path(path, "Countries_December_2019_Boundaries_UK_BFE", 
+                       "Countries_December_2019_Boundaries_UK_BFE.shp")
+uk <- rgdal::readOGR(shapeFile)
