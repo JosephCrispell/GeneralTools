19bfb96af286724969fb402859769bea9cbb85ae
JosephCrispell <crispelljoseph@gmail.com>
2020-05-11 20:14:54 +0100

    Working on Wicklow spoligotyping

--- Spoligotyping/ExamineWicklowSpotypingResults_11-05-20.R
+++ Spoligotyping/ExamineWicklowSpotypingResults_11-05-20.R
@@ -3,13 +3,13 @@
 #### Read in the data ####
 
 # Set the path
-path <- file.path("~", "storage", "Research", "RepublicOfIreland", "Mbovis", "Wicklow", "Spoligotyping")
+path <- file.path("~", "storage", "Research", "RepublicOfIreland", "Mbovis", "Wicklow")
 
 # Read in the Wicklow FASTQ spoligotypes (created using "RunSpoTypingOnFASTQs_05-11-20.sh")
-types <- read.table(file.path(path, "SpotypingOutput_11-05-20.txt"), header=TRUE, sep="\t", colClasses="character")
+types <- read.table(file.path(path, "Spoligotyping", "SpotypingOutput_11-05-20.txt"), header=TRUE, sep="\t", colClasses="character")
 
 # Read in the conversion table
-conversion <- read.csv(file.path(path, "Mbovis_SpoligotypeCodes_11-05-20.csv"), colClasses="character")
+conversion <- read.csv(file.path(path, "Spoligotyping", "Mbovis_SpoligotypeCodes_11-05-20.csv"), colClasses="character")
 conversion$Binary <- gsub(pattern=" ", replacement="", x=conversion$Binary)
 
 #### Retrieve spoligotypes ####
@@ -30,4 +30,13 @@ types$ID <- sapply(types$Files,
                      return(strsplit(fileName, split="_")[[1]][1])
                    })
 
-types[, c("ID", "Spoligotype")]
+#### Look at spoligotypes for genomes included in manuscript ####
+
+# Read in the SRA submission table
+sra<- read.table(file.path(path, "SRA_submission_18-11-19", "Biosample_Wicklow_18-11-19.tsv"), header=TRUE,
+                 sep="\t", stringsAsFactors=FALSE)
+
+# Get spoligotypes for submitted genomes
+types <- types[types$ID %in% sra$X.sample_name, ]
+
+types[, c("ID", "Spoligotype", "Code")]
